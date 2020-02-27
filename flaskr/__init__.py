d29ae1aa7bdc112f6e220f12e24915187ea0eb43
Will Kocur <will.kocur@unep-wcmc.org>
2020-02-27 20:29:47 +0000

    refactor db functions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -39,7 +39,6 @@ def create_app(test_config=None):
         conn = db.get_db()
         with conn:
           if not get_model(conn, model_id):
-            create_model(conn, (model_id, 0))
             init_model(conn, model_id)
           else:
             update_model(conn, data)
@@ -61,34 +60,30 @@ def create_app(test_config=None):
           'model_id': model_id,
           'state': state
         }
-    
-    def get_model(conn, model_id):
-      sql = "SELECT * FROM model WHERE id = ?"
-      cur = conn.cursor()
-      cur.execute(sql, (model_id))
-      data = cur.fetchall()
 
-      if len(data) == 0:
-        return None
-      else:
-        return data[0]
+    def get_cell_model_join_id(conn, cell_number, model_id):
+      cell_model_join = get_cell_model_join(conn, cell_number, model_id)
 
-    def get_cell_id(conn, cell_number, model_id):
-      sql = "SELECT id FROM cell_model_join WHERE model_id = ? AND cell_number = ?"
+      return cell_model_join[0][0] if len(cell_model_join) != 0 else None
+
+    def get_cell_model_join(conn, cell_number, model_id):
+      sql = "SELECT * FROM cell_model_join WHERE model_id = ? AND cell_number = ?"
       cur = conn.cursor()
       cur.execute(sql, (model_id, cell_number))
-      cell = cur.fetchall()
+      cell_model_join = cur.fetchall()
 
-      if len(cell) == 0:
-        sql = "INSERT INTO cell_model_join (cell_number, model_id) VALUES (?,?)"
-        cur = conn.cursor()
-        cur.execute(sql, (cell_number, model_id))
-        return cur.lastrowid
-      else:
-        return cell[0]
+      return cell_model_join if len(cell_model_join) != 0 else None
 
+    def create_cell(conn, cell_number, model_id):
+      sql = "INSERT INTO cell_model_join (cell_number, model_id) VALUES (?,?)"
+      cur = conn.cursor()
+      cur.execute(sql, (cell_number, model_id))
+      return cur.lastrowid
+    
     def init_model(conn, model_id):
-      init_model_state(conn, model_id)
+      create_model(conn, model_id)
+      init_timestamp_model_join(conn, model_id)
+
       grid_state = SimpleMadingleyModel.ReturnInitialGrid()
       for cell_index in range(len(grid_state['herbivore_biomasses'])):
         cell_state_values_for_db = ()
@@ -102,7 +97,7 @@ def create_app(test_config=None):
           else:
             cell_state_values_for_db += (cell_value,)
 
-        cell_id = get_cell_id(conn, cell_index + 1, model_id)
+        cell_id = create_cell(conn, cell_index + 1, model_id)
 
         sql = '''INSERT INTO timestamp_cell_join (
                 cell_id, 
@@ -117,17 +112,33 @@ def create_app(test_config=None):
         cur = conn.cursor()
         cur.execute(sql, (cell_id, 0,) + cell_state_values_for_db)
 
+    def get_model(conn, model_id):
+      sql = "SELECT * FROM model WHERE id = ?"
+      cur = conn.cursor()
+      cur.execute(sql, (model_id))
+      data = cur.fetchall()
+
+      return data[0] if len(data) != 0 else None
+
     def update_model(conn, data):
       print('Update model', data)
 
-    def create_model(conn, model):
+    def create_model(conn, model_id):
       sql = ''' INSERT INTO model (id, time_elapsed) 
                 VALUES (?,?) '''
       cur = conn.cursor()
-      cur.execute(sql, model)
+      cur.execute(sql, (model_id, 0))
       return cur.lastrowid
 
-    def init_model_state(conn, model_id):
+    def get_timestamp_model_join(conn, model_id, timestamp):
+      sql = "SELECT * FROM timestamp_model_join WHERE model_id = ? and timestamp = ?"
+      cur = conn.cursor()
+      cur.execute(sql, (model_id, timestamp))
+      data = cur.fetchall()
+
+      return data[0] if len(data) != 0 else None
+
+    def init_timestamp_model_join(conn, model_id):
       sql = ''' INSERT INTO timestamp_model_join (model_id, timestamp, temperature) 
                 VALUES (?,?,?) '''
       cur = conn.cursor()
